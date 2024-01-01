4dee5bc007e2c407c5e1809018f9c712ae10e774
Peter Lef <lefp@clarkson.edu>
2024-01-01 01:13:48 -0500

    Separate compile and link

--- build.sh
+++ build.sh
@@ -1,25 +1,56 @@
 #!/bin/sh
 
+echo 'building -----------------------------------------------------------------------'
+
 # grep --recursive --line-number 'FIXME' src
 # if [ $? -eq 0 ] ; then
 #   echo '^ You have unresolved FIXMEs. Aborting build.'
 #   exit
 # fi
 
+
+set DEBUG_FLAG=-g3
+
+
+ls -d build &> /dev/null && rm -r build
+mkdir -p build
+mkdir -p build/intermediate_objects
+
+
 ls src | grep -E '\.(vert|frag|comp)$' | while IFS= read -r src_file ; do
   glslc -o build/"$src_file".spv src/"$src_file"
 done
 
+
+echo 'Compiling libs/loguru/loguru.cpp'
 g++ \
-  -Werror -Wall -Wextra \
-  -Walloc-zero -Wcast-qual -Wconversion -Wduplicated-branches -Wduplicated-cond -Wfloat-equal -Wformat=2 \
-  -Wformat-signedness -Winit-self -Wlogical-op -Wmissing-declarations -Wshadow -Wswitch -Wundef \
-  -Wunused-result -Wwrite-strings -Wsign-conversion \
-  -Wno-missing-field-initializers \
-  $(pkg-config --cflags glfw3) \
-  $(pkg-config --static --libs glfw3) \
-  -isystem libs \
-  -g3 \
-  -o build/test \
+  $DEBUG_FLAG \
+  -c \
+  -I libs/loguru \
   libs/loguru/loguru.cpp \
-  src/*.cpp
+  -o build/intermediate_objects/loguru.cpp.o
+
+
+ls src | grep -E '\.cpp$' | while IFS= read -r src_file ; do
+  echo "Compiling $src_file"
+  g++ \
+    -Werror -Wall -Wextra \
+    -Walloc-zero -Wcast-qual -Wconversion -Wduplicated-branches -Wduplicated-cond -Wfloat-equal -Wformat=2 \
+    -Wformat-signedness -Winit-self -Wlogical-op -Wmissing-declarations -Wshadow -Wswitch -Wundef \
+    -Wunused-result -Wwrite-strings -Wsign-conversion \
+    -Wno-missing-field-initializers \
+    $DEBUG_FLAG \
+    -isystem libs \
+    -c \
+    src/"$src_file" \
+    -o build/intermediate_objects/"$src_file".o
+done
+
+echo "Linking"
+g++ \
+    $(pkg-config --static --libs glfw3) \
+    build/intermediate_objects/*.o \
+    -lc -ldl \
+    -o build/test
+
+echo 'done ---------------------------------------------------------------------------'
