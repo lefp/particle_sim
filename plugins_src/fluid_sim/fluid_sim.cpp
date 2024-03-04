eaff7513526ba00e4019cb42a15b52b48201f537
Peter Lef <lefp@clarkson.edu>
2024-03-04 16:52:19 -0500

    Fluid sim: partial new data structure implementation

--- plugins_src/fluid_sim/fluid_sim.cpp
+++ plugins_src/fluid_sim/fluid_sim.cpp
@@ -2,7 +2,10 @@
 #include <cstdlib>
 #include <cinttypes>
 #include <cmath>
+#include <signal.h> // @debug TODO delete this
+#include <stdio.h> // @debug TODO delete this
 
+#define GLM_FORCE_EXPLICIT_CTOR
 #include <glm/glm.hpp>
 #include <loguru/loguru.hpp>
 
@@ -10,11 +13,13 @@
 #include "../src/error_util.hpp"
 #include "../src/math_util.hpp"
 #include "../src/alloc_util.hpp"
+#include "../src/defer.hpp" // @debug TODO delete this
 #include "fluid_sim_types.hpp"
 
 namespace fluid_sim {
 
 using glm::vec3;
+using glm::uvec3;
 
 //
 // ===========================================================================================================
@@ -25,6 +30,16 @@ constexpr f32 PI = (f32)M_PI;
 //
 // ===========================================================================================================
 //
+
+#define SWAP(a, b) \
+{ \
+    static_assert(typeof(a) == typeof(b)); \
+    \
+    typeof(a) _tmp = a; \
+    a = b; \
+    b = _tmp; \
+}
+
 
 extern "C" void setParams(SimData* s, const SimParameters* params) {
     s->parameters.rest_particle_density = params->rest_particle_density;
@@ -53,7 +68,301 @@ extern "C" void setParams(SimData* s, const SimParameters* params) {
 }
 
 
-extern "C" SimData create(const SimParameters* params, u32fast particle_count, const vec3* p_initial_positions) {
+/// Get the index of the cell that contains the particle.
+static inline uvec3 cellIndex(vec3 particle, vec3 domain_min, f32 cell_size_reciprocal) {
+
+    return uvec3((domain_min + particle) * cell_size_reciprocal);
+}
+
+
+static inline u32 cellMortonCode(uvec3 cell_index) {
+    return
+        ((cell_index.x & 0b0000000001) <<  0) |
+        ((cell_index.y & 0b0000000001) <<  1) |
+        ((cell_index.z & 0b0000000001) <<  2) |
+        ((cell_index.x & 0b0000000010) <<  2) |
+        ((cell_index.y & 0b0000000010) <<  3) |
+        ((cell_index.z & 0b0000000010) <<  4) |
+        ((cell_index.x & 0b0000000100) <<  4) |
+        ((cell_index.y & 0b0000000100) <<  5) |
+        ((cell_index.z & 0b0000000100) <<  6) |
+        ((cell_index.x & 0b0000001000) <<  6) |
+        ((cell_index.y & 0b0000001000) <<  7) |
+        ((cell_index.z & 0b0000001000) <<  8) |
+        ((cell_index.x & 0b0000010000) <<  8) |
+        ((cell_index.y & 0b0000010000) <<  9) |
+        ((cell_index.z & 0b0000010000) << 10) |
+        ((cell_index.x & 0b0000100000) << 10) |
+        ((cell_index.y & 0b0000100000) << 11) |
+        ((cell_index.z & 0b0000100000) << 12) |
+        ((cell_index.x & 0b0001000000) << 12) |
+        ((cell_index.y & 0b0001000000) << 13) |
+        ((cell_index.z & 0b0001000000) << 14) |
+        ((cell_index.x & 0b0010000000) << 14) |
+        ((cell_index.y & 0b0010000000) << 15) |
+        ((cell_index.z & 0b0010000000) << 16) |
+        ((cell_index.x & 0b0100000000) << 16) |
+        ((cell_index.y & 0b0100000000) << 17) |
+        ((cell_index.z & 0b0100000000) << 18) |
+        ((cell_index.x & 0b1000000000) << 18) |
+        ((cell_index.y & 0b1000000000) << 19) |
+        ((cell_index.z & 0b1000000000) << 20) ;
+}
+
+
+static inline u32 mortonCodeHash(u32 cell_morton_code, u32 hash_modulus) {
+    return cell_morton_code % hash_modulus;
+}
+
+
+/// Merge sort the particles by their Morton codes.
+/// If the result is written to a scratch buffer, swaps the scratch buffer pointer with the appropriate data
+/// buffer pointer.
+static void mergeSortByMortonCodes(
+    const u64 arr_size,
+    vec3 **const pp_positions,
+    vec3 **const pp_velocities,
+    vec3 **const pp_scratch1,
+    vec3 **const pp_scratch2,
+    const vec3 domain_min,
+    const f32 cell_size_reciprocal
+) {
+
+    vec3* pos_arr1 = *pp_positions;
+    vec3* pos_arr2 = *pp_scratch1;
+
+    vec3* vel_arr1 = *pp_velocities;
+    vec3* vel_arr2 = *pp_scratch2;
+
+    assert(arr_size >= 2);
+
+    for (u64 bucket_size = 1; bucket_size < arr_size; bucket_size *= 2)
+    {
+        const u64 bucket_count = arr_size / bucket_size;
+
+        for (u64 bucket_idx = 0; bucket_idx < bucket_count; bucket_idx += 2)
+        {
+            u64 idx_a = (bucket_idx    ) * bucket_size;
+            u64 idx_b = (bucket_idx + 1) * bucket_size;
+            u64 idx_dst = idx_a;
+
+            const u64 idx_a_max = glm::min(idx_a + bucket_size, arr_size);
+            const u64 idx_b_max = glm::min(idx_b + bucket_size, arr_size);
+            const u64 idx_dst_max = glm::min(idx_dst + 2*bucket_size, arr_size);
+
+            for (; idx_dst < idx_dst_max; idx_dst++)
+            {
+                u32 morton_code_a;
+                if (idx_a < idx_a_max) {
+                    vec3 particle_a = pos_arr1[idx_a];
+                    const uvec3 cell_a = cellIndex(particle_a, domain_min, cell_size_reciprocal);
+                    morton_code_a = cellMortonCode(cell_a);
+                }
+                else morton_code_a = UINT32_MAX;
+
+                u32 morton_code_b;
+                if (idx_b < idx_b_max) {
+                    vec3 particle_b = pos_arr1[idx_b];
+                    const uvec3 cell_b = cellIndex(particle_b, domain_min, cell_size_reciprocal);
+                    morton_code_b = cellMortonCode(cell_b);
+                }
+                else morton_code_b = UINT32_MAX;
+
+                if (morton_code_a < morton_code_b) {
+                    pos_arr2[idx_dst] = pos_arr1[idx_a];
+                    vel_arr2[idx_dst] = vel_arr1[idx_a];
+                    idx_a++;
+                }
+                else {
+                    pos_arr2[idx_dst] = pos_arr1[idx_b];
+                    vel_arr2[idx_dst] = vel_arr1[idx_b];
+                    idx_b++;
+                }
+            }
+        }
+
+        vec3* tmp = pos_arr1;
+        pos_arr1 = pos_arr2;
+        pos_arr2 = tmp;
+
+        tmp = vel_arr1;
+        vel_arr1 = vel_arr2;
+        vel_arr2 = tmp;
+    }
+
+    *pp_positions = pos_arr1;
+    *pp_velocities = vel_arr1;
+    *pp_scratch1 = pos_arr2;
+    *pp_scratch2 = vel_arr2;
+}
+
+
+static void mergeSortByCellHashes(
+    const u64 array_size,
+    u32 **const pp_cells,
+    u32 **const pp_lengths,
+    u32 **const pp_scratch1,
+    u32 **const pp_scratch2,
+    const vec3 *const p_particles,
+    const vec3 domain_min,
+    const f32 cell_size_reciprocal,
+    const u32 hash_modulus
+) {
+
+    u32* cells_arr1 = *pp_cells;
+    u32* cells_arr2 = *pp_scratch1;
+
+    u32* lens_arr1 = *pp_lengths;
+    u32* lens_arr2 = *pp_scratch2;
+
+    assert(array_size >= 2);
+
+    for (u64 bucket_size = 1; bucket_size < array_size; bucket_size *= 2)
+    {
+        const u64 bucket_count = array_size / bucket_size;
+
+        for (u64 bucket_idx = 0; bucket_idx < bucket_count; bucket_idx += 2)
+        {
+            u64 idx_a = (bucket_idx    ) * bucket_size;
+            u64 idx_b = (bucket_idx + 1) * bucket_size;
+            u64 idx_dst = idx_a;
+
+            const u64 idx_a_max = glm::min(idx_a + bucket_size, array_size);
+            const u64 idx_b_max = glm::min(idx_b + bucket_size, array_size);
+            const u64 idx_dst_max = glm::min(idx_dst + 2*bucket_size, array_size);
+
+            for (; idx_dst < idx_dst_max; idx_dst++)
+            {
+                u32 cell_hash_a;
+                if (idx_a < idx_a_max) {
+                    const u32 particle_idx = cells_arr1[idx_a];
+                    const uvec3 cell = cellIndex(p_particles[particle_idx], domain_min, cell_size_reciprocal);
+                    const u32 morton_code = cellMortonCode(cell);
+                    cell_hash_a = mortonCodeHash(morton_code, hash_modulus);
+                }
+                else cell_hash_a = UINT32_MAX;
+
+                u32 cell_hash_b;
+                if (idx_b < idx_b_max) {
+                    const u32 particle_idx = cells_arr1[idx_b];
+                    const uvec3 cell = cellIndex(p_particles[particle_idx], domain_min, cell_size_reciprocal);
+                    const u32 morton_code = cellMortonCode(cell);
+                    cell_hash_b = mortonCodeHash(morton_code, hash_modulus);
+                }
+                else cell_hash_b = UINT32_MAX;
+
+                if (cell_hash_a < cell_hash_b) {
+                    cells_arr2[idx_dst] = cells_arr1[idx_a];
+                    lens_arr2[idx_dst] = lens_arr1[idx_a];
+                    idx_a++;
+                }
+                else {
+                    cells_arr2[idx_dst] = cells_arr1[idx_b];
+                    lens_arr2[idx_dst] = lens_arr1[idx_b];
+                    idx_b++;
+                }
+            }
+        }
+
+        u32* tmp = cells_arr1;
+        cells_arr1 = cells_arr2;
+        cells_arr2 = tmp;
+
+        tmp = lens_arr1;
+        lens_arr1 = lens_arr2;
+        lens_arr2 = tmp;
+    }
+
+    *pp_cells = cells_arr1;
+    *pp_lengths = lens_arr1;
+    *pp_scratch1 = cells_arr2;
+    *pp_scratch2 = lens_arr2;
+}
+
+
+static inline bool isPrime(u32fast n) {
+
+    if (n == 0 || n == 1) return false;
+
+    for (u32fast i = 2; i < n; i++)
+    {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+
+static u32fast getNextPrimeNumberExclusive(u32fast n) {
+
+    if (n == 0 || n == 1) return 2;
+
+    n++; // never return input
+    if (!(n & 1)) n++; // if even, make it odd
+
+    // maybe this is 1 too large, that's fine
+    const u32fast max = (u32fast)(ceil(sqrt((f64)n)) + 0.5);
+
+    while (true)
+    {
+        bool prime = true;
+        for (u32fast i = 3; i <= max; i++)
+        {
+            if (n % i == 0)
+            {
+                prime = false;
+                break;
+            }
+        }
+        if (prime) return n;
+        n += 2;
+    }
+}
+
+
+struct CompactCell {
+    u32 first_particle_idx;
+    u32 particle_count;
+};
+static inline CompactCell particle2Cell(const SimData* s, const vec3 particle, const vec3 domain_min) {
+
+    const uvec3 cell_idx_3d = cellIndex(particle, domain_min, s->cell_size_reciprocal);
+    const u32 morton_code = cellMortonCode(cell_idx_3d);
+    const u32 hash = mortonCodeHash(morton_code, s->hash_modulus);
+
+    const u32 first_cell_with_hash_idx = s->H_begin[hash];
+    const u32 n_cells_with_hash = s->H_length[hash];
+
+    assert(n_cells_with_hash > 0); // you must not pass nonexistent particles to this function
+
+    u32 cell_idx = first_cell_with_hash_idx;
+    const u32 cell_idx_end = cell_idx + n_cells_with_hash;
+
+    for (; cell_idx < cell_idx_end; cell_idx++)
+    {
+        const u32 first_particle_in_cell_idx = s->p_cells[cell_idx];
+        assert(first_particle_in_cell_idx < s->particle_count);
+
+        const vec3 first_particle_in_cell = s->p_positions[first_particle_in_cell_idx];
+        if (
+            cellMortonCode(cellIndex(first_particle_in_cell, domain_min, s->cell_size_reciprocal))
+            == morton_code
+        ) {
+            return CompactCell {
+                .first_particle_idx = first_particle_in_cell_idx,
+                .particle_count = s->p_cell_lengths[cell_idx],
+            };
+        }
+    }
+
+    return CompactCell { .first_particle_idx = UINT32_MAX, .particle_count = 0 };
+}
+
+
+extern "C" SimData create(
+    const SimParameters* params,
+    u32fast particle_count,
+    const vec3* p_initial_positions
+) {
 
     SimData s {};
     {
@@ -64,7 +373,29 @@ extern "C" SimData create(const SimParameters* params, u32fast particle_count, c
 
         s.p_velocities = callocArray(particle_count, vec3);
 
+        s.p_particles_scratch_buffer1 = callocArray(particle_count, vec3);
+        s.p_particles_scratch_buffer2 = callocArray(particle_count, vec3);
+
+        s.cell_count = 0;
+        s.p_cells = callocArray(particle_count + 1, u32);
+        s.p_cell_lengths = callocArray(particle_count, u32);
+
+        s.p_cells_scratch_buffer1 = callocArray(particle_count + 1, u32);
+        s.p_cells_scratch_buffer2 = callocArray(particle_count + 1, u32);
+
+        // smallest prime number larger than the maximum number of particles
+        // OPTIMIZE profile this and optimize if too slow
+        u32fast hash_modulus = getNextPrimeNumberExclusive(particle_count);
+        assert(hash_modulus <= UINT32_MAX);
+        s.hash_modulus = (u32)hash_modulus;
+
+        s.H_begin = callocArray(hash_modulus, u32);
+        s.H_length = callocArray(hash_modulus, u32);
+
         setParams(&s, params);
+
+        s.cell_size = 2.0f * s.parameters.particle_interaction_radius;
+        s.cell_size_reciprocal = 1.0f / s.cell_size;
     }
 
     LOG_F(INFO, "Initialized fluid sim with %" PRIuFAST32 " particles.", s.particle_count);
@@ -83,19 +414,163 @@ extern "C" void destroy(SimData* s) {
 
 extern "C" void advance(SimData* s, f32 delta_t) {
 
+    // @debug TODO delete this
+    FILE* logfile = fopen("tmp.log", "w");
+    defer(fclose(logfile));
+
     assert(delta_t > 1e-5); // assert nonzero
 
     const u32fast particle_count = s->particle_count;
     const f32 particle_interaction_radius = s->parameters.particle_interaction_radius;
     const f32 spring_stiffness = s->parameters.spring_stiffness;
     const f32 spring_rest_length = s->parameters.spring_rest_length;
+    const f32 cell_size_reciprocal = s->cell_size_reciprocal;
+
+    vec3 domain_min = vec3(INFINITY);
+    vec3 domain_max = vec3(-INFINITY);
+    for (u32fast i = 0; i < particle_count; i++)
+    {
+        domain_min = glm::min(s->p_positions[i], domain_min);
+        domain_max = glm::max(s->p_positions[i], domain_max);
+    }
+
+    {
+        uvec3 cell_count = uvec3(glm::ceil((domain_max - domain_min) * cell_size_reciprocal) + 0.5f);
+        // 32-bit Morton codes can handle at most a 1024x1024x1024 grid.
+        // If this turns out to be insufficient, consider using 64-bit Morton codes.
+        assert(cell_count.x < 1024 and cell_count.y < 1024 and cell_count.z < 1024);
+    }
+
+    mergeSortByMortonCodes(
+        particle_count,
+        &s->p_positions, &s->p_velocities, &s->p_particles_scratch_buffer1, &s->p_particles_scratch_buffer2,
+        domain_min, cell_size_reciprocal
+    );
+
+    // @debug TODO delete this
+    fprintf(logfile, "Particle Morton codes:\n");
+    for (u32fast i = 0; i < s->particle_count; i++)
+    {
+        fprintf(
+            logfile, "%lu: %u\n",
+            i,
+            cellMortonCode(cellIndex(s->p_positions[i], domain_min, cell_size_reciprocal))
+        );
+    }
+    fflush(logfile);
+
+    // fill cell list
+    {
+        u32 prev_morton_code = 0;
+        if (particle_count > 0)
+        {
+            s->p_cells[0] = 0;
+            prev_morton_code = cellMortonCode(cellIndex(s->p_positions[0], domain_min, cell_size_reciprocal));
+        }
+
+        u32fast cell_idx = 1;
+        for (u32fast particle_idx = 1; particle_idx < particle_count; particle_idx++)
+        {
+            u32 morton_code =
+                cellMortonCode(cellIndex(s->p_positions[particle_idx], domain_min, cell_size_reciprocal));
+
+            if (morton_code == prev_morton_code) continue;
+
+            prev_morton_code = morton_code;
+
+            s->p_cells[cell_idx] = (u32)particle_idx;
+
+            cell_idx++;
+        }
+        s->p_cells[cell_idx] = (u32)particle_count;
+
+        const u32fast cell_count = cell_idx;
+        s->cell_count = cell_count;
+
+        for (cell_idx = 0; cell_idx < cell_count; cell_idx++)
+        {
+            s->p_cell_lengths[cell_idx] = (u32)s->p_cells[cell_idx+1] - s->p_cells[cell_idx];
+        }
+
+        mergeSortByCellHashes(
+            cell_count,
+            &s->p_cells, &s->p_cell_lengths, &s->p_cells_scratch_buffer1, &s->p_cells_scratch_buffer2,
+            s->p_positions,
+            domain_min, cell_size_reciprocal,
+            s->hash_modulus
+        );
+
+        // @debug TODO delete this
+        fprintf(logfile, "Cell hashes:\n");
+        for (u32fast i = 0; i < s->cell_count; i++)
+        {
+            fprintf(
+                logfile, "%lu: %u\n",
+                i,
+                mortonCodeHash(cellMortonCode(cellIndex(s->p_positions[s->p_cells[i]], domain_min, cell_size_reciprocal)), s->hash_modulus)
+            );
+        }
+        fflush(logfile);
+    }
+
+    {
+        for (u32fast i = 0; i < s->hash_modulus; i++) s->H_begin[i] = UINT32_MAX;
+        for (u32fast i = 0; i < s->hash_modulus; i++) s->H_length[i] = 0;
+
+        const u32 cell_count = (u32)s->cell_count;
+
+        s->H_begin[0] = 0;
+        if (cell_count > 0) s->H_length[0] = 1;
+        u32 prev_hash;
+        {
+            const u32 particle_idx = s->p_cells[0];
+            const vec3 particle = s->p_positions[particle_idx];
+            const uvec3 cell_idx_3d = cellIndex(particle, domain_min, cell_size_reciprocal);
+            const u32 morton_code = cellMortonCode(cell_idx_3d);
+            prev_hash = mortonCodeHash(morton_code, s->hash_modulus);
+        }
+        u32 hash = UINT32_MAX;
+        u32 cells_with_this_hash_count = 1;
+
+        for (u32 cell_idx = 1; cell_idx < cell_count; cell_idx++)
+        {
+            const u32 particle_idx = s->p_cells[cell_idx];
+            const vec3 particle = s->p_positions[particle_idx];
+            const uvec3 cell_idx_3d = cellIndex(particle, domain_min, cell_size_reciprocal);
+            const u32 morton_code = cellMortonCode(cell_idx_3d);
+            hash = mortonCodeHash(morton_code, s->hash_modulus);
+
+            if (hash != prev_hash)
+            {
+                s->H_begin[hash] = cell_idx;
+                s->H_length[prev_hash] = cells_with_this_hash_count;
+
+                // OPTIMIZE: if `cells_with_this_hash_count == 1`, set `H_begin[prev_hash] = cell_idx-1`.
+                // Make sure you account for this embedding in lookups.
+
+                cells_with_this_hash_count = 0;
+                prev_hash = hash;
+            }
+            cells_with_this_hash_count++;
+        }
+        s->H_length[hash] = cells_with_this_hash_count;
+    }
 
     for (u32fast i = 0; i < particle_count; i++)
     {
         vec3 accel_i = vec3(0);
         vec3 pos_i = s->p_positions[i];
 
-        for (u32fast j = 0; j < particle_count; j++)
+        const CompactCell cell = particle2Cell(s, pos_i, domain_min);
+        // the cell must exist, because our particle is in it.
+        assert(cell.first_particle_idx != UINT32_MAX);
+        assert(cell.particle_count > 0);
+
+        u32fast j = cell.first_particle_idx;
+        const u32fast j_end = j + cell.particle_count;
+
+        // TODO FIXME also apply forces of particles in adjacent cells
+        for (; j < j_end; j++)
         {
             if (i == j) continue;
 
--- plugins_src/fluid_sim/fluid_sim_types.hpp
+++ plugins_src/fluid_sim/fluid_sim_types.hpp
@@ -24,12 +24,30 @@ struct SimData {
     vec3* p_positions;
     vec3* p_velocities;
 
+    vec3* p_particles_scratch_buffer1;
+    vec3* p_particles_scratch_buffer2;
+
+    u32* p_cells_scratch_buffer1;
+    u32* p_cells_scratch_buffer2;
+
+    // This is `\mathbb{C}_compact^begin` in the paper "Multi-Level Memory Structures for Simulating and
+    // Rendering Smoothed Particle Hydrodynamics" by Winchenbach and Kolb.
+    u32fast cell_count;
+    u32* p_cells;
+    u32* p_cell_lengths; // `\mathbb{C}_compact^length
+
+    u32 hash_modulus;
+    u32* H_begin;
+    u32* H_length;
+
     struct {
         f32 rest_particle_density;
         f32 particle_interaction_radius;
         f32 spring_rest_length;
         f32 spring_stiffness;
     } parameters;
+    f32 cell_size; // edge length
+    f32 cell_size_reciprocal;
 };
 
 //
