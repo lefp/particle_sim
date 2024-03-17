lefp/particle_sim

commit 0f1982ff1f6dfc38ccf1c5432d4482e5fbb794f1

Fix lack of ccache for Tracy build

build.py
@@ -106,9 +106,11 @@ with open('build/env_vars.hpp', 'w') as f:
 #     It would be nice to have a file named F_compileTracy so that other build scripts can add `F` to the
 #     `dependsOn` lists in their filenames.
 if (common.isTracyEnabled()):
+    print("Building Tracy")
     result: sp.CompletedProcess = sp.run(
         [
             'g++',
+            '-c', # note: ccache doesn't work without the -c flag
             '-fPIC', '-shared',
             '-o', './build/tracy.so',
             'libs/tracy/TracyClient.cpp',

commit 41517a2c07b1c72f4928d5d7d2eef7639963a14c

Fix fluid sim Tracy linking
Bug was introduced in commit 0f1982ff1f6dfc38ccf1c5432d4482e5fbb794f1

build.py
@@ -111,8 +111,8 @@ if (common.isTracyEnabled()):
         [
             'g++',
             '-c', # note: ccache doesn't work without the -c flag
-            '-fPIC', '-shared',
-            '-o', './build/tracy.so',
+            '-fPIC',
+            '-o', './build/tracy.o',
             'libs/tracy/TracyClient.cpp',
             '-isystem', 'libs/tracy',
             '-std=c++11',
@@ -121,6 +121,18 @@ if (common.isTracyEnabled()):
         + common.getCompilerFlag_O()
         + ['-DNDEBUG']
     )
+    result.check_returncode()
+    result = sp.run(
+        [
+            'g++',
+            '-fPIC', '-shared',
+            './build/tracy.o',
+            '-o', './build/tracy.so',
+            '-isystem', 'libs/tracy',
+            '-std=c++11',
+        ]
+    )
+    result.check_returncode()
 
 # TODO whichever stages can be done in parallel, do those in parallel (using Popen)
 